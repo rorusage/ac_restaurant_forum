b96ce00a56a6d532835df854050d2a1cd83cd5ac
rorusage <rorusage@gmail.com>
2017-12-06 17:07:34 +0800

    Create category data and  an admin user as seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Category
+
+Category.destroy_all
+
+category_list = [
+  { name: "中式料理"},
+  { name: "日本料理"},
+  { name: "義大利料理"},
+  { name: "墨西哥料理"},
+  { name: "素食料理"},
+  { name: "美式料理"},
+  { name: "複合式料理"}
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name])
+end
+puts "Category created!"
+
+# Default admin
+
+User.create(email: "root@example.com", password: "12345678", role: "admin")
+puts "Default admin created!"
