e66e5774f10db2980cfce65f84f3f9befba50272
rorusage <rorusage@gmail.com>
2017-12-20 18:14:43 +0800

    User can update the profile data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
+  end
+
+  def edit
+    unless @user == current_user
+      redirect_to user_path(@user), alert: "你不是本人！無權編輯！"
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :intro)
   end
 end
