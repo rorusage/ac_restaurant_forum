8967081bf1b6a64717f19d7833a14261921a59b2
rorusage <rorusage@gmail.com>
2017-12-21 18:17:33 +0800

    User can view commented restaurants on profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :comments
+  has_many :restaurants, through: :comments
   mount_uploader :avatar, AvatarUploader
   validates_presence_of :name
 
