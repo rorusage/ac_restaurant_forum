0c4d217540cfa29ba180cad81c14ccfb9653e466
rorusage <rorusage@gmail.com>
2017-12-21 17:46:15 +0800

    Add user name to registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parmeters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parmeters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :comments
   mount_uploader :avatar, AvatarUploader
+  validates_presence_of :name
 
   def admin?
     self.role == "admin"
