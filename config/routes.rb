b08e04b83c4c58a943a4b9c4d68a814bbb4dd0f8
rorusage <rorusage@gmail.com>
2018-01-03 17:53:03 +0800

    Setup routes for feeds and dashboard pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
+    collection do
+      get :feeds
+    end
+
+    member do
+      get :dashboard
+    end
   end
   resources :users, only: [:show, :edit, :update]
   resources :categories, only: :show
