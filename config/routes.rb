f6b92bb612d3878b2d79b9ff31c069a5a7775049
rorusage <rorusage@gmail.com>
2018-01-11 17:30:04 +0800

    Setup routes for like and unlike

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
       get :dashboard
       post :favorite
       post :unfavorite
+
+      post :like
+      post :unlike
     end
   end
   resources :users, only: [:show, :edit, :update]
