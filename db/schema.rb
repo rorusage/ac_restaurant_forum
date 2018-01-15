333a89f67a75d8ca3f44688ca7f6df155167fd26
rorusage <rorusage@gmail.com>
2018-01-15 17:03:58 +0800

    Add favorites_count to restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180111093124) do
+ActiveRecord::Schema.define(version: 20180115090139) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -47,10 +47,11 @@ ActiveRecord::Schema.define(version: 20180111093124) do
     t.string   "address"
     t.string   "opening_hours"
     t.text     "description"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "image"
     t.integer  "category_id"
+    t.integer  "favorites_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|
