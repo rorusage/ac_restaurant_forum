f9b9f2d4742b3d095658f68e832d2448d92a8e44
rorusage <rorusage@gmail.com>
2018-01-22 17:51:27 +0800

    Create followship model and setup self-referential relationships within users table through followships

---
+++ db/migrate/20180122093739_create_followships.rb
@@ -0,0 +1,9 @@
+class CreateFollowships < ActiveRecord::Migration
+  def change
+    create_table :followships do |t|
+      t.integer :user_id
+      t.integer :following_id
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115090139) do
+ActiveRecord::Schema.define(version: 20180122093739) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20180115090139) do
     t.integer  "restaurant_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "followships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "following_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "likes", force: :cascade do |t|
