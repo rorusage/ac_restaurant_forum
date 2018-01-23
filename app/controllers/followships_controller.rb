2d9a9d0d00def503a2af9156a8e2928b4823f9f8
rorusage <rorusage@gmail.com>
2018-01-23 17:23:39 +0800

    Add create action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,2 +1,12 @@
 class FollowshipsController < ApplicationController
+  def create
+    @followship = current_user.followships.build(following_id: params[:following_id])
+    if @followship.save
+      flash[:notice] = "Successfully followed"
+      redirect_to :back
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_to :back
+    end
+  end
 end
