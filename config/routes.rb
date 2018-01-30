13c400001bd30ad7a7862dbf54d375989aed4663
rorusage <rorusage@gmail.com>
2018-01-30 17:03:11 +0800

    Setup routes for user index page and friend / unfriend function

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:index, :show, :edit, :update]
   resources :followships, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
   resources :categories, only: :show
   root "restaurants#index"
 
